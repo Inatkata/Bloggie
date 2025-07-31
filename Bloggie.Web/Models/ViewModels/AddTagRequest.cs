6ea4fa37616e83b849abf96e200d8e79c72db0b9
Ina <inatkata@yahoo.com>
2025-07-31 08:39:08 +0200

    Add project files.

---
+++ Bloggie.Web/Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
